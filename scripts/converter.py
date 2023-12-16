2156b32d99cadee630056d8ee2c0389b70f1f718
Rndlab <suruotai@gmail.com>
2023-12-16 15:55:01 +0800

    [add] 增加excel转json的exe

--- scripts/converter.py
+++ scripts/converter.py
@@ -1,9 +1,13 @@
 import json
 import pandas as pd
 import math
+import time
+from pypinyin import Style, lazy_pinyin
 song_df = pd.read_excel('./music.xlsx')
 song_df = song_df.where(pd.notnull(song_df), None)
 song_list = []
+
+
 print('开始生成歌单')
 for index, row in song_df.iterrows():
     print(row[0])
@@ -13,15 +17,15 @@ for index, row in song_df.iterrows():
         "artist": row[1] or '',
         "language": row[2] or '中文',
         "remarks": '' if row[3] is None else row[3],
-        "initial": row[4] or 'A',
-        "sticky_top": row[5] or 0,
-        "paid": row[6] or 0,
-        "BVID": '' if row[7] is None else row[7]
+        "initial": ''.join(lazy_pinyin(row[0], style=Style.FIRST_LETTER))[0].upper(),
+        "sticky_top": 0,
+        "paid": 0,
+        "BVID": '' if row[4] is None else row[4]
     }
-    if row[5] == 1:
-        song_list.insert(0,song_data)
-    else:
-        song_list.append(song_data)
+    song_list.append(song_data)
 
 with open("../public/music_list.json", 'w') as f:
     f.write(json.dumps(song_list))
+print('生成歌单完成')
+time.sleep(5000)
+
